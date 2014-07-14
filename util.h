1378a6c2ce599a01de33e330b738a33f37762f82
William Langford <wlangfor@gmail.com>
2014-07-13 21:31:19 -0400

    Moving memmem to util.c

--- util.h
+++ util.h
@@ -6,4 +6,22 @@
 jv canonicalize_path(jv);
 jv get_home(void);
 
+#ifndef HAVE_MEMMEM
+const void *memmem(const void *haystack, size_t haystacklen,
+                   const void *needle, size_t needlelen);
+#endif /* HAVE_MEMMEM */
+
+#ifndef MIN
+#define MIN(a,b) \
+  ({ __typeof__ (a) _a = (a); \
+   __typeof__ (b) _b = (b); \
+   _a < _b ? _a : _b; })
+#endif
+#ifndef MAX
+#define MAX(a,b) \
+  ({ __typeof__ (a) _a = (a); \
+   __typeof__ (b) _b = (b); \
+   _a > _b ? _a : _b; })
+#endif
+
 #endif /* UTIL_H */
